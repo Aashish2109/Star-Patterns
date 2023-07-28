40e20fee3c2dca0de7a0025c924c29c9519dce7f
Aashish Gupta <aashishg863@gmail.com>
2023-07-28 14:27:08 +0530

    startpattern

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int rows,columns;
+    cout<<"Enter the number of rows :"<<endl;
+    cin>>rows;
+    cout<<"Enter the number of columns :"<<endl;
+    cin>>columns;
+    for(int i=0;i<rows;i++)
+    {
+        for(int j=i;j<columns;j++)
+        {
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+}
